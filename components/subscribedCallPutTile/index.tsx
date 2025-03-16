1897b3131bb15b14a553dc9da0ec42912730753f
Sandstorm831 <gargrohan138@gmail.com>
2025-03-16 15:59:41 +0530

    added refactoring in realtime page

---
+++ components/subscribedCallPutTile/index.tsx
@@ -0,0 +1,51 @@
+export default function SubscribedCallPutTile({
+  callPut,
+  dataIdx,
+  text_colr,
+  subscribeByIdx,
+  idx,
+}: {
+  callPut: string;
+  dataIdx: number[] | null;
+  text_colr: string | undefined;
+  subscribeByIdx: (row: number, col: number) => void;
+  idx: number;
+}) {
+  const price_ind = callPut === "C" ? 1 : 3;
+  const change_ind = callPut === "C" ? 0 : 4;
+  const opt_type = callPut === "C" ? "Call" : "Put";
+  const sub_ind = callPut === "C" ? 0 : 1;
+  return (
+    <div className="flex flex-col p-2 bg-gray-50 rounded-lg h-full justify-center mt-2 font-mono">
+      <div className="flex justify-center font-bold text-xl">
+        {dataIdx ? `${dataIdx[2]}${callPut}` : null}
+      </div>
+      <div className="flex justify-center text-sm">
+        {dataIdx ? `Option Type : ${opt_type}` : null}
+      </div>
+      <div className="flex justify-center text-sm">
+        {dataIdx ? `Option Strike : ${dataIdx[2]}` : null}
+      </div>
+      <div className="flex justify-center text-sm whitespace-pre-wrap">
+        <div>Option Price : </div>
+        <div className={`text-sm ${text_colr}`}>
+          {dataIdx ? dataIdx[price_ind] : null}
+        </div>
+      </div>
+      <div className="flex justify-center text-sm whitespace-pre-wrap">
+        <div>Price Change : </div>
+        <div className={`text-sm ${text_colr}`}>
+          {dataIdx ? dataIdx[change_ind] : null}%
+        </div>
+      </div>
+      <div
+        className="w-full flex justify-center text-white font-bold bg-blue-800 rounded-lg mt-2 cursor-pointer"
+        onClick={() => subscribeByIdx(idx, sub_ind)}
+      >
+        Unsubscribe
+      </div>
+    </div>
+  );
+}
+
+// data[idx] => dataIdx
